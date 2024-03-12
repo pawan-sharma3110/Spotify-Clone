db436a6379bced60661a05118cf60dab726f09be
pawan-sharma3110 <pawansharma93520@gmail.com>
2024-03-12 23:38:16 +0530

    java script function debuging is left

--- index.js
+++ index.js
@@ -6,22 +6,19 @@ var myProgressBar = document.getElementById('myProgressBar');
 var songItems = Array.from(document.getElementsByClassName('songName'));
 var songs =[
 {songName: "Salam-e-Ishq", filePath: "songs/1.mp3", coverPath:"covers/1.jpg"},
-{songName: "Salam-e-Ishq", filePath: "songs/2.mp3", coverPath:"covers/2.jpg"},
-{songName: "Salam-e-Ishq", filePath: "songs/3.mp3", coverPath:"covers/3.jpg"},
-{songName: "Salam-e-Ishq", filePath: "songs/4.mp3", coverPath:"covers/4.jpg"},
-{songName: "Salam-e-Ishq", filePath: "songs/5.mp3", coverPath:"covers/5.jpg"},
-{songName: "Salam-e-Ishq", filePath: "songs/6.mp3", coverPath:"covers/6.jpg"},
-{songName: "Salam-e-Ishq", filePath: "songs/7.mp3", coverPath:"covers/7.jpg"},
-{songName: "Salam-e-Ishq", filePath: "songs/8.mp3", coverPath:"covers/8.jpg"},
-{songName: "Salam-e-Ishq", filePath: "songs/9.mp3", coverPath:"covers/9.jpg"},
-{songName: "Salam-e-Ishq", filePath: "songs/10.mp3", coverPath:"covers/10.jpg"},
+{songName: "Ishq-rishk", filePath: "songs/2.mp3", coverPath:"covers/2.jpg"},
+{songName: "Sanam-re", filePath: "songs/3.mp3", coverPath:"covers/3.jpg"},
+{songName: "Tera-fitoor", filePath: "songs/4.mp3", coverPath:"covers/4.jpg"},
+{songName: "Sanam-teri-kasam", filePath: "songs/5.mp3", coverPath:"covers/5.jpg"},
+{songName: "Qismat", filePath: "songs/6.mp3", coverPath:"covers/6.jpg"},
+{songName: "Struglear", filePath: "songs/7.mp3", coverPath:"covers/7.jpg"},
+{songName: "Love-me-like-do", filePath: "songs/8.mp3", coverPath:"covers/8.jpg"},
+{songName: "A-man-without-love", filePath: "songs/9.mp3", coverPath:"covers/9.jpg"},
+{songName: "Majboriyan", filePath: "songs/10.mp3", coverPath:"covers/10.jpg"},
 
 ];
 
-songItems.forEach((Element,i)=>{
-Element.getElementsByTagName("img")[0].src = song[i].filePath;
-});
-  
+
 masterPlay.addEventListener('click',()=>{
     if (audioElement.paused || audioElement.currentTime<=0) {
         audioElement.play();
@@ -47,4 +44,20 @@ audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
 audioElement.play();
 masterPlay.innerHTML = "pause";
 gif.style.opacity="5";
+})
+
+
+
+var makeAllPlay =()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((Element)=>{
+        Element.innerHTML="play_arrow";
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+        makeAllPlay();
+        index = parseInt(e.target.id);
+        e.target;
+    })
 })
